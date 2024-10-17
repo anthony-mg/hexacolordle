19d6adf99e22e181f7ecaf3011cffbd39f798671
Anthony Menard-Gill <anthonymenardgill@gmail.com>
2024-10-17 13:21:50 -0400

    added menus, refined mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import Game from "./components/Game";
+import Game from "./components/game_screen/Game";
+import { useState } from "react";
+import MainMenu from "./components/main_screen/MainMenu";
+import HowTo from "./components/how_to/HowTo";
+export enum Status {
+  MAIN_MENU,
+  HOW_TO,
+  GAME,
+}
 
 const App = () => {
-  return (
-    <>
-      <Game />
-    </>
-  );
+  function renderScreen(screen: Status) {
+    switch (screen) {
+      case Status.MAIN_MENU:
+        return <MainMenu setScreen={setScreen} />;
+      case Status.HOW_TO:
+        return <HowTo setScreen={setScreen} />;
+      case Status.GAME:
+        return <Game />;
+
+      default:
+        console.assert(false, "This should never happen");
+    }
+  }
+  const [screen, setScreen] = useState<Status>(Status.MAIN_MENU);
+
+  return <>{renderScreen(screen)}</>;
 };
 
 export default App;
